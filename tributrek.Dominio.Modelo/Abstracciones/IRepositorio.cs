9fb505ba933953553ef9c312554b2a8345c4f1b5
leslieraquel <raquellezs@hotmail.com>
2025-07-05 17:43:33 -0500

    CLASES GENERICAS

--- tributrek.Dominio.Modelo/Abstracciones/IRepositorio.cs
+++ tributrek.Dominio.Modelo/Abstracciones/IRepositorio.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace tributrek.Dominio.Modelo.Abstracciones
 {
-    internal interface IRepositorio
+    public interface IRepositorio<T> where T : class
     {
+
+        Task AddAsync(T TEntity); /// para insertar
+        Task UpdateAsync(T TEntity); /// actualizar
+
+        Task DeleteAsync(int id); /// delete 
+
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByAsync(int id);
+
     }
+
 }
--- tributrek.Infraestructura.AccesoDatos/Repositorio/RepositorioImpl.cs
+++ tributrek.Infraestructura.AccesoDatos/Repositorio/RepositorioImpl.cs
@@ -1,12 +1,82 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tributrek.Dominio.Modelo.Abstracciones;
 
 namespace tributrek.Infraestructura.AccesoDatos.Repositorio
 {
-    internal class RepositorioImpl
+    public class RepositorioImpl<T> : IRepositorio<T> where T : class
     {
+        private readonly tributrekContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+        public async Task AddAsync(T TEntity)
+        {
+            try
+            {
+                await _dbSet.AddAsync(TEntity);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error: No se pudo insertar" + e.Message);
+            }
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            try
+            {
+                var entity = await GetByAsync(id);
+                _dbSet.Remove(entity);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error: No se pudo eliminar los datos" + e.Message);
+            }
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            try
+            {
+                return await _dbSet.ToListAsync();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error: No se pudo listar los datos" + e.Message);
+            }
+        }
+
+        public async Task<T> GetByAsync(int id)
+        {
+            try
+            {
+                var entity = GetByAsync(id);
+                return await _dbSet.FindAsync(entity);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error: No se encontro nigun registro" + e.Message);
+            }
+        }
+
+        public async Task UpdateAsync(T TEntity)
+        {
+            try
+            {
+                _dbSet.Update(TEntity);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error: No se pudo eliminar los datos" + e.Message);
+            }
+        }
     }
+
+   
 }
