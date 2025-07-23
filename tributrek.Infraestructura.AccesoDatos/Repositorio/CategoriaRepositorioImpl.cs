9ceb566f449dd3178be671832dccc8d994d3c621
Jose Pillajo <josepilljo004@gmail.com>
2025-07-23 11:52:10 -0500

    cambios en categorias y actividades

--- tributrek.Infraestructura.AccesoDatos/Repositorio/CategoriaRepositorioImpl.cs
+++ tributrek.Infraestructura.AccesoDatos/Repositorio/CategoriaRepositorioImpl.cs
@@ -16,7 +16,50 @@ namespace tributrek.Infraestructura.AccesoDatos.Repositorio
         {
             this._tributrekContext = dBContext;
         }
-   
+
+        public async Task AgregarCategoriaAsync(tri_categoria categoria)
+        {
+            try
+            {
+                _tributrekContext.tri_categoria.Add(categoria);
+                await _tributrekContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al agregar la categoría: " + ex.Message);
+            }
+        }
+
+        public async Task ActualizarCategoriaAsync(tri_categoria categoria)
+        {
+            try
+            {
+                _tributrekContext.tri_categoria.Update(categoria);
+                await _tributrekContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al actualizar la categoría: " + ex.Message);
+            }
+        }
+
+        public async Task EliminarCategoriaAsync(int idCategoria)
+        {
+            try
+            {
+                var categoria = await _tributrekContext.tri_categoria.FindAsync(idCategoria);
+                if (categoria != null)
+                {
+                    _tributrekContext.tri_categoria.Remove(categoria);
+                    await _tributrekContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al eliminar la categoría: " + ex.Message);
+            }
+        }
+
         public async  Task<List<CategoriaDTO>> ListarCategorias()
         {
             try
